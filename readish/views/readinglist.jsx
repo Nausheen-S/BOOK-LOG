0c470c6912a7c64615e94a663d637eb170015c3a
Nausheen_S <salauddinnausheen@gmail.com>
2020-04-29 21:12:07 +0800

    added logic to view reading list

---
+++ readish/views/readinglist.jsx
@@ -0,0 +1,26 @@
+var React = require('react');
+
+class ReadingList extends React.Component {
+  render() {
+
+   /* const readingList = this.props.books.map((book)=>{
+        //const url  = `/about/${book.id}`;
+        return (<li>{book.book_id}</li>)
+
+    })*/
+    console.log(this.props.id);
+    return (
+      <html>
+        <body>
+          <h1>My Reading List</h1>
+          <ul>
+          {this.props.books}
+
+          </ul>
+        </body>
+      </html>
+    );
+  }
+}
+
+module.exports = ReadingList;
